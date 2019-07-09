22755ba95b4ac839fc8c3420ba5b24d03a55f7ae
wei zhang <wei.zhang@windstoneservice.com>
2019-07-09 13:24:43 +0800

    feign demo

---
+++ spring-cloud-feign/eureka-client/src/main/java/cn/waynezw/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,27 @@
+package cn.waynezw.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @GetMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "wayne") String name) {
+        return "Hello " + name + ", access port:" + port;
+    }
+}
